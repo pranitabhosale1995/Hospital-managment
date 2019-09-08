347cbaf9bfdb6255b0e8c66085976d7f7cc3c48c
pari <pranitabhosale1995@gmail.com>
2019-09-08 15:24:54 +0530

    new config

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,12 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 
-
 module.exports = (env, argv) => {
   const isProd = argv.mode === "production";
   const config = {};
   config.mode = argv.mode;
 
-
-  config.entry = [path.join(__dirname, "/src/index.js")];
+  config.entry = [path.join(__dirname, "/src/index.jsx")];
   config.output = {
     path: path.join(__dirname, "/dist"),
     filename: isProd ? "[name].[hash].js" : "[name].js"
@@ -26,16 +24,20 @@ module.exports = (env, argv) => {
 
   config.module = {
     rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
       },
-    },
-    {
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-      loader: "url-loader?limit=100000",
-    }
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000",
+      }
     ]
   };
 
@@ -46,41 +48,46 @@ module.exports = (env, argv) => {
     config.devtool = false;
 
     config.plugins.push(
+
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        template: path.join(__dirname, "/HTML_TEMPLATE/index.html"),
+        template: path.join(__dirname, "/SRC_TEMPLATE/index.html"),
         minify: {
           collapseWhitespace: true,
           minifyCSS: true
         },
-      })
+      }),
     );
   } else {
     config.devtool = "eval";
-
     config.plugins.push(
       new HtmlWebpackPlugin({
-        template: path.join(__dirname, "/HTML_TEMPLATE/index.html"),
+        template: path.join(__dirname, "/SRC_TEMPLATE/index.html"),
         minify: {
           collapseWhitespace: false,
-          minifyCSS: true
+          minifyCSS: false
         },
       }),
+
     );
+
+
+
   }
 
   config.plugins.push(
     new CopyWebpackPlugin(
       [{
-        from: path.join(__dirname, "/HTML_TEMPLATE")
+        from: path.join(__dirname, "/SRC_TEMPLATE")
       }], {
         ignore: ["*.html", "service-worker.js"]
       }
     )
   );
 
+
   config.devServer = {
-    port: 3004,
+    port: 4321,
     stats: "minimal"
   };
 
